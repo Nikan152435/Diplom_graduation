8620ca9504550fd2ad9b35402486bf1bbbcd4c84
NikaN <NikaN7602@gmail.com>
2024-08-16 20:54:56 +0200

    Добавлены новые файлы и изменения в проект: - Оформлен README.md - Добавлены HTML, CSS и JavaScript файлы - Добавлены конфигурационные файлы - Обновлены файлы для проекта

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './js/client.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+          { from: 'images', to: 'images' },
+          { from: 'css', to: 'css' },
+          { from: 'js', to: 'js' },
+      ],
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
